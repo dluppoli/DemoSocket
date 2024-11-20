eae347e868cc4edb807160beb6c098044e803270
Davide Luppoli <davide@davideluppoli.it>
2024-11-20 11:26:50 +0100

    Add project files.

---
+++ DemoTCPClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTCPClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creo una nuova socket
+            var address = new IPEndPoint(IPAddress.Loopback, 8080);
+            using (Socket serverSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                //Collegarsi al server
+                await serverSocket.ConnectAsync(address);
+
+                //Creare lo stream di comunicazione
+                using (var stream = new NetworkStream(serverSocket))
+                {
+                    //Invia i dati al server
+                    while(true)
+                    {
+                        Console.Write("Inserire il messaggio da inviare (EXIT per uscire: ");
+                        string message = Console.ReadLine();
+                        if (message == "EXIT") break;
+
+                        byte[] buffer = Encoding.ASCII.GetBytes(message);
+                        await stream.WriteAsync(buffer, 0, buffer.Length);
+
+                        //Riceve la risposta dal server
+                        byte[] responseBuffer = new byte[1024];
+                        await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length);
+                        string responseMessage = Encoding.ASCII.GetString(responseBuffer);
+                        Console.WriteLine("Il server ha risposto con: " + responseMessage);
+                    } 
+                    //Chiudere socker e stream
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoTCPServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTCPServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creare la socket
+            var address = new IPEndPoint(IPAddress.Loopback, 8080);
+            Socket serverSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //Bindind della socket verso IP e Porta
+            serverSocket.Bind(address);
+            //Mettere in ascolto il server
+            serverSocket.Listen(100);
+
+            //Accettare la connessione
+            while (true)
+            {
+                using (Socket clientSocket = await serverSocket.AcceptAsync())
+                {
+                    //Generare lo stream di comunicazione
+                    using (NetworkStream stream = new NetworkStream(clientSocket))
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                //Ricevere dati
+                                byte[] buffer = new byte[1024];
+                                await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                                //Elabora i dati
+                                string message = Encoding.ASCII.GetString(buffer);
+                                Console.WriteLine("Ho ricevuto " + message);
+
+                                //Spedisce dati di risposta
+                                message = "Echo di: " + message;
+                                byte[] responseBuffer = Encoding.ASCII.GetBytes(message);
+                                await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
+                            }
+                            catch (Exception ex)
+                            {   
+                                Console.WriteLine(ex.ToString());
+                                break;
+                            }
+                        }
+                        //Chiusura della socket e dello stream
+                    }
+                }
+            }
+//            Console.WriteLine("Esecuzione terminata");
+//            Console.ReadLine();
+        }
+    }
+}
