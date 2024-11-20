76201426d06f443c1d9a0f27b9288a7c98ce61d8
Davide Luppoli <davide@davideluppoli.it>
2024-11-20 12:30:22 +0100

    Versioni UDP

---
+++ DemoUDPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUDPClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var address = new IPEndPoint(IPAddress.Loopback, 8081);
+            using(var clientSocket = new UdpClient() )
+            {
+                clientSocket.Connect(address);
+
+                while (true)
+                {
+                    Console.WriteLine("Inserire il messaggio (EXIT per uscire): ");
+                    string message = Console.ReadLine();
+                    if (message == "EXIT") break;
+
+                    byte[] buffer = Encoding.ASCII.GetBytes(message);
+                    await clientSocket.SendAsync(buffer, buffer.Length);
+
+                    var response = await clientSocket.ReceiveAsync();
+                    message = Encoding.ASCII.GetString(response.Buffer);
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ DemoUDPServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoUDPServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var address = new IPEndPoint(IPAddress.Loopback, 8081);
+            using (UdpClient serverSocket = new UdpClient(address))
+            {
+                while (true)
+                {
+                    var result = await serverSocket.ReceiveAsync();
+                    byte[] buffer = result.Buffer;
+
+                    string message = Encoding.ASCII.GetString(buffer);
+
+                    Thread.Sleep(3000);
+
+                    message = "Echo di : " + message;
+
+                    byte[] responseBuffer = Encoding.ASCII.GetBytes(message);
+                    await serverSocket.SendAsync(responseBuffer, responseBuffer.Length,result.RemoteEndPoint);
+                }
+            }
+        }
+    }
+}
